71312e352ffef1ed789d42ccbe372cb720af3fc8
Simão Durão <simaodurao@gmail.com>
2025-03-12 01:44:40 +0000

    Added Special to the sln and did the recursion

---
+++ Special/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Special
+{
+    public class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            if (n==0)
+            {
+                return 0;
+            }
+            else if (n==1)
+            {
+                return  1;
+            }
+            else
+            {   
+                return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+            }
+        }
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(n));
+        }
+    }
+}

0d935837c0ed08be624ec64e061e1ede87e81927
Simão Durão <simaodurao@gmail.com>
2025-03-12 00:50:05 +0000

    Added BetterDecorador to the sln

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static string Decor(string a, char b, int c)
+        {
+            string decor = new string(b, c);
+            return $"{decor} {a} {decor}";
+        }
+
+        private static void Main(string[] args)
+        {
+            string str = args[0];
+            char Char = char.Parse(args[1]);
+            Console.WriteLine(Decor(str,Char,int.Parse(args[2])));
+        }
+
+    }
+}
